839f62e2137274909d94276b387a78670e085513
Goster210 <juan.rincon12@uptc.edu.co>
2023-03-14 20:34:32 -0500

    configuracion inicial tablero de juego

---
+++ src/Components/GameBoard.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+
+function GameBoard() {
+    return (
+      <div >
+        <h1>GAME BOARD</h1>
+  
+      </div>
+    );
+  }
+  
+  export default GameBoard;
+  
